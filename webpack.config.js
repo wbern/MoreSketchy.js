a58c60b702d25090541214698b1944f384040c12
William Bernting <william.bernting@kits.se>
2017-04-18 10:11:14 +0200

    don't uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 // const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); // to access built-in plugins
+// const webpack = require('webpack'); // to access built-in plugins
 const path = require('path');
 
 const config = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.min.js'
+    filename: 'index.js'
   },
   module: {
     rules: [
       {test: /\.(js|jsx)$/, use: 'babel-loader'}
     ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+  }
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin()
+  // ]
 };
 
 module.exports = config;
